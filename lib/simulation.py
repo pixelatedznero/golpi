c3bc45b6111ea08efb3e11bcef22aef79869467d
Znero <pixelatedznero@gmail.com>
2023-02-23 11:58:16 +0100

    extrafunktions for centerpattern and creatboard

--- lib/simulation.py
+++ lib/simulation.py
@@ -22,21 +22,36 @@ class Simulation:
 
         return twod_list
 
-    def run(self, inputpattern, boardsize=(100,100), patternposition=None):
+
+    def centerpattern(self, pattern, boardsize):
+        patternposition = (int(boardsize[0]/2),int(boardsize[1]/2))
+
+        patternposition = (patternposition[0]-int(len(pattern[0])/2),
+                           patternposition[1]-int(len(pattern)/2))
+        
+        return patternposition
+
+
+    def creatboard(self, pattern, boardsize=(100,100), patternposition=None):
         if patternposition == None:
-            patternposition = (int(boardsize[0]/2), int(boardsize[1]/2))
-        patternposition = (patternposition[0]-int(len(inputpattern)/2), patternposition[1]-int(len(inputpattern[0])/2))
+            patternposition = self.centerpattern(pattern, boardsize)
+
         board = []
+
         for i in range(boardsize[0]):
             board.append([])
             for j in range(boardsize[1]):
-                if i < patternposition[0] or i > patternposition[0]+len(inputpattern)-1:
+                if i < patternposition[0] or i > patternposition[0]+len(pattern)-1:
                     board[i].append(0)
-                elif j < patternposition[1] or j > patternposition[1]+len(inputpattern[1])-1:
+                elif j < patternposition[1] or j > patternposition[1]+len(pattern[1])-1:
                     board[i].append(0)
                 else:
-                    board[i].append(inputpattern[i-patternposition[0]][j-patternposition[1]])
+                    board[i].append(pattern[i-patternposition[0]][j-patternposition[1]])
 
+        return board
+
+
+    def run(self, board):
         for g in range(2):
             
             print(board[43][43:49])
@@ -97,7 +112,7 @@ class Simulation:
 
 gm = Simulation()
 
-gm.run(gm.convert2twod([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
+gm.run(gm.creatboard(gm.convert2twod([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                         0,1,1,0,0,0,0,0,0,0,0,0,0,1,0,
                         0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,
                         0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,
@@ -112,7 +127,7 @@ gm.run(gm.convert2twod([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
                         0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,
                         0,1,1,1,0,0,0,0,0,0,0,0,1,1,0,
                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-                       15)
+                       15))
        )
 
 print("dada")
