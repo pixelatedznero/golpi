c0ff1696e275c1e9edd92f303135f387412cf986
flexicoding <>
2023-03-24 10:33:00 +0100

    Added python functions that reference the c functions #5

--- test.py
+++ test.py
@@ -1,35 +1,6 @@
-#import golpi, time
-from ctypes import *
+from golpi.c_impl.golpi_c import *
 
-golpi_c_functions = CDLL("build/optimizedsim.so")
-test_board = b'      *        ***                *                     *              *       ***                 *'
+raw_data = b'      *        ***                *                     *              *       ***                 *'
 
-golpi_c_functions.update_board(test_board)
-golpi_c_functions.print_board(test_board)
-
-
-"""
-
-- (converter 1d to 2d)
-- python board object to 
-- addpattern
-
-"""
-
-
-
-
-# times = []
-
-# for i in range(5):
-#     before = time.time()
-
-#     board = golpi.createboard((100,100))
-#     for i in range(20):
-#         board.add([[1,1,1]], (i,i))
-
-#     board.simulate(500)
-
-#     times.append(time.time()-before)
-
-# print(sum(times)/len(times))
+board = golpi_c_create_board(raw_data, 10, 10, BORDER_MODE_TRANSPARENT)
+golpi_c_print_board(pointer(board))
